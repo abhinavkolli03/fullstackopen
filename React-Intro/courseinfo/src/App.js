f595389fe1afe42ace17ac85af065dccc1395663
Abhinav Kolli <abhinavkolli03@gmail.com>
2022-05-17 17:25:01 -0500

    finished practice content

--- React-Intro/courseinfo/src/App.js
+++ React-Intro/courseinfo/src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const Header = (props) => {
+  return(
+    <div>
+      <h1>{props.course.name}</h1>
     </div>
-  );
+  )
+}
+
+const Content = (props) => {
+  const parts = props.course.parts.map((part) => 
+    <p>{part.name} {part.exercises}</p>)
+  return(
+    <div>
+      {parts}
+    </div>
+  )
+}
+
+const Total = (props) => {
+  let total = 0
+  props.course.parts.forEach(element => {
+    total += element.exercises
+    console.log(element)
+  });
+  return(
+    <div>
+      <p>{total} exercises total</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Header course={course}/>
+      <Content course={course}/>
+      <Total course={course}/>
+    </div>
+  )
 }
 
 export default App;
--- React-Intro/practice-content/src/App.js
+++ React-Intro/practice-content/src/App.js
@@ -1,27 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
-const Hello = (props) => {
+const History = (props) => {
+  if(props.allClicks.length === 0) {
+    return(
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
   return (
     <div>
-      <p>Hello {props.name}. You are {props.age} years old.</p>
+      button press history: {props.allClicks.join(' ')}
     </div>
   )
 }
 
-const App = () => {
-  const a = 10
-  const b = 20
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
 
-  console.log('hello from component')
+const App = () => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+  }
+
   return (
-    <div className="App">
-      <p>Greetings</p>
-      <Hello name="George" age={a}/>
-      <Hello name="Abhinav" age={b}/>
-      <p>
-        {a} plus {b} is {a+b}
-      </p>
+    <div>
+      {left}
+      <Button handleClick={handleLeftClick} text='left' />
+      <Button handleClick={handleRightClick} text='right' />
+      {right}
+      <History allClicks={allClicks} />
     </div>
   )
 }
