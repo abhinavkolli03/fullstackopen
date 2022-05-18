7a23e59f3468f133303fb460b54f02df0dbef8b8
Abhinav Kolli <abhinavkolli03@gmail.com>
2022-05-18 18:25:57 -0500

    updated phonebook and added axios and useeffect in practice-content

--- Server-Communication/practice-content/src/App.js
+++ Server-Communication/practice-content/src/App.js
@@ -1,12 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 import Note from './components/Note'
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
+import axios from 'axios'
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
+const App = () => {
+  const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        console.log('promise fulfilled')
+        setNotes(response.data)
+      })
+  }, [])
+  console.log('render', notes.length, 'notes')
 
   const addNote = (event) => {
     event.preventDefault()
@@ -40,7 +52,7 @@ const App = (props) => {
           <Note key={note.id} note={note}/>
         )}
       </ul>
-      <form onsubmit={addNote}>
+      <form onSubmit={addNote}>
           <input value={newNote} onChange={handleNoteChange}/>
           <button type="submit">save</button>
       </form>
