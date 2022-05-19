021eb8b838c12c9c1adc4add948a7bd627f2f487
Abhinav Kolli <abhinavkolli03@gmail.com>
2022-05-19 18:32:04 -0500

    Finished practice-content for part 2

--- Server-Communication/practice-content/src/App.js
+++ Server-Communication/practice-content/src/App.js
@@ -2,17 +2,25 @@ import './App.css';
 import Note from './components/Note'
 import {useState, useEffect} from 'react'
 import noteService from './services/notes'
+import Notification from './components/Notification'
+import Footer from './components/Footer'
 
 const App = () => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState(null)
 
   useEffect(() => {
     noteService
       .getAll()
       .then(initialNotes => {
-        setNotes(initialNotes)
+        setNotes(initialNotes.concat({
+          id: 10000,
+          content: 'This note is not saved to server',
+          date: '2019-05-30T17:30:31.098Z',
+          important: true,
+        }))
       })
   }, [])
 
@@ -26,9 +34,12 @@ const App = () => {
         setNotes(notes.map(note => note.id !== id ? note : returnedNote))
       })
       .catch(error => {
-        alert(
-          `the note ${note.content} was already deleted from server`
+        setErrorMessage(
+          `Note \"${note.content}\" was already deleted from server`
         )
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
         setNotes(notes.filter(n => n.id !== id))
       })
   }
@@ -58,6 +69,7 @@ const App = () => {
   return(
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage} />
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all'}
@@ -76,6 +88,7 @@ const App = () => {
           <input value={newNote} onChange={handleNoteChange}/>
           <button type="submit">save</button>
       </form>
+      <Footer />
     </div>
   )
 }
